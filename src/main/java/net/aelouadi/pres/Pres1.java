9ae785ee3f816d778dfae7349aae5699fb9e2f84
aelouadi <abdelalielouadi8@gmail.com>
2026-03-16 16:30:48 +0100

    add presentation class

--- src/main/java/net/aelouadi/pres/Pres1.java
+++ src/main/java/net/aelouadi/pres/Pres1.java
@@ -1,13 +1,16 @@
 package net.aelouadi.pres;
 
+import net.aelouadi.dao.DaoImpl;
+import net.aelouadi.dao.IDao;
 import net.aelouadi.metier.IMetier;
 import net.aelouadi.metier.MetierImpl;
 
 public class Pres1 {
 
     public static void main(String[] args) {
-        IMetier metier = new MetierImpl();
-
+        IDao dao = new DaoImpl();
+        MetierImpl metier = new MetierImpl();
+        metier.setDao(dao);
         System.out.println("RES = " + metier.calacul());
     }
 }
